9804948f61d185fa62ed771b1aef1d31fc1f0c1b
Leonardo F Paixão <vpsleo@gmail.com>
2022-09-30 21:30:12 -0300

    atualizado30.09

--- forms.py
+++ forms.py
@@ -1,15 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de Usuario', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao = PasswordField('Confirmacao', validators=[DataRequired(), EqualTo('senha')])
+    confirmacao_senha = PasswordField('Confirmacao', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar conta')
 
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar dados de acesso')
     botao_submit_login = SubmitField('Fazer Login')
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash, redirect
+from forms import FormLogin, FormCriarConta
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = '6598859202a577053dac9703e4810d0f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///comunidade.db'
+
+database = SQLAlchemy(app)
 
 lista_usuarios = ['Leonardo' , 'Thamyres', 'Gabriel']
 
@@ -18,9 +25,17 @@ def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucessono e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criado com e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
 
